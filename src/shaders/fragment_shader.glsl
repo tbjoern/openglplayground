e320aeebdd71cdc5151210e189b1ba294630d596
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2018-03-02 17:43:26 +0100

    Revert "make pretty reflections"
    This reverts commit 5bbd60bf7a14025b8ecf6f9aa0ac20b3d993688a.

--- src/shaders/fragment_shader.glsl
+++ src/shaders/fragment_shader.glsl
@@ -6,23 +6,14 @@ in vec3 Color;
 
 out vec4 out_color;
 
-// uniform sampler2D eliteTex;
+uniform sampler2D eliteTex;
 uniform sampler2D kittenTex;
 
 uniform float time;
 
 void main()
 {
-    float xcoord = Texture.x;
-    float ycoord = Texture.y;
-    if(Texture.y > 0.5f) {
-        float sin_distort = (sin(Texture.y * 50.f + time) + sin(Texture.y* 80.f)) / 30.f;
-        sin_distort *= (ycoord - 0.5f);
-        xcoord += sin_distort;
-        ycoord = 1.f - ycoord;
-    }
-    vec2 texture_distorted = vec2(xcoord, ycoord);
-    // vec4 colElite = texture(eliteTex, Texture);
-    vec4 colKitten = texture(kittenTex, texture_distorted);
-    out_color = colKitten;
+    vec4 colElite = texture(eliteTex, Texture);
+    vec4 colKitten = texture(kittenTex, Texture);
+    out_color = mix(colElite, colKitten, time);
 }
